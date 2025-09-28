31089e2acf49f4e05e3482c246fc5d193712d1ca
Alan Fica <alan.fica2101@alumnos.ubiobio.cl>
2025-09-28 01:11:05 -0300

    Update index.js
    actualizacion index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
-import { AppDataSource, connectDB } from "./config/configDB.js";
+import { AppDataSource, connectDB } from "./config/configDb.js";
 import { routerApi } from "./routes/index.routes.js";
+import { HOST, PORT} from "./config/configEnv.js";
 
 const app = express();
 app.use(express.json());
@@ -21,7 +22,7 @@ connectDB()
     // Levanta el servidor Express
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(`Servidor iniciado en http://localhost:${PORT}`);
+      console.log(`Servidor iniciado en http://${HOST}:${PORT}`);
     });
   })
   .catch((error) => {
