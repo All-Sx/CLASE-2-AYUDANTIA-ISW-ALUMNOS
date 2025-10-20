f7782ed18e9977da121cebd8c80758d9985dd79c
Alan Fica <alan.fica2101@alumnos.ubiobio.cl>
2025-10-19 22:49:21 -0300

    Solucion cors
    Agregada la ruta para realizar validaciones al cors (antes solo permitia la ruta local).

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,8 @@ import { HOST, PORT} from "./config/configEnv.js";
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({origin: ["http://localhost:5173","http://146.83.198.35:1351"],credentials: true}));
+
 // Ruta principal de bienvenida
 app.get("/", (req, res) => {
   res.send("¡Bienvenido a mi API REST con TypeORM!");
